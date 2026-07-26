39d94b4aba20492c21c3186e1606ab737434c858
teganomalley9 <teganomalley9@gmail.com>
2026-07-26 16:49:44 -0400

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -1137,7 +1137,6 @@ if (continueWatching) {
 
 const recentlyFinished = document.getElementById("recentlyFinished");
 
-    document.getElementById("recentlyFinished")
 ;
 if (recentlyFinished) {
     
